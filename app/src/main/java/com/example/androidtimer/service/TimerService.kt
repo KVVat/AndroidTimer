fa22c4b04ac45a02d9547e4917226a96cc81f9fe
wkouki <kkww413@gmail.com>
2019-11-12 18:10:15 +0900

    first commit

--- app/src/main/java/com/example/androidtimer/service/TimerService.kt
+++ app/src/main/java/com/example/androidtimer/service/TimerService.kt
@@ -3,12 +3,19 @@ package com.example.androidtimer.service
 import android.app.IntentService
 import android.content.Intent
 import android.util.Log
+import android.widget.Toast
 
 class TimerService :IntentService(TimerService::class.simpleName){
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Toast.makeText(this,"serviceStarting",Toast.LENGTH_SHORT).show()
+        return super.onStartCommand(intent, flags, startId)
+    }
     override fun onHandleIntent(workIntent: Intent?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        Log.i("Observe","Testing")
+        if (workIntent != null) {
+            val data = workIntent?.getStringExtra("data")
+            Log.i("TAG","Message is : $data")
+        }
     }
 
 }
